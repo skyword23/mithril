056c4ede2cbfb7dfef59232d9a0c89d7f323e7d3
skyword23 <harishcarya@gmail.com>
2018-05-16 14:42:53 +0530

    refactoring

--- index.js
+++ index.js
@@ -1,110 +1,61 @@
 import m from "mithril";
 
+//  class encapulating 'data'
+
 class Data  {
 
     constructor() {
         this.data = [];
-        this.start = 1;
-        this.pageSize = 10;
-        this.max = 1000000;
-
         // generate dummy data for displaying
-
-        for(let k=1; k <= this.max; k++) {
+        
+        var max = 1000;
+        for(let k=1; k <= max; k++) {
             this.data.push({ind : k, locn: 1700 + k, ordid: "ORD_" + k, ksn: 1500 + k, sku : 10000});
         }
     }
-
-    // return a subset of objects based on current value of 'start' and 'page size' 
-
-    getViewData(){
-    
-        var viewData = [];
-        var end = Math.min(data.start -1 + data.pageSize, data.max);
-
-        for(var k=data.start-1; k < end; k++){
-            viewData.push(data.data[k]);
-        }
-        return viewData;
-    }
-
-    nextRow(){
-        this.start = Math.min(this.start + 1, this.max);
-    }
-
-    prevRow(){
-        this.start = Math.max(this.start - 1, 1);
-    }
-    
-    nextPage(){
-
-        this.start += this.pageSize;
-        if (this.start > this.max){
-            this.start -= this.pageSize;
-        }
-    }
-    prevPage() {
-        this.start -= this.pageSize;
-        if (this.start < 1){
-            this.start += this.pageSize;
-        }
-    }
-    
-    lastPage() {
-        this.start = Math.max(this.max - this.pageSize + 1, 1);
-    }
-
-    firstPage(){
-        this.start = 1;
-    }
 };
+
+// instantiate object containng data
 
 var data = new Data();
 
-function nextRow(){
+// Button click hadlers for navigation
 
-    data.nextRow();
-    m.mount(root, view);
+function nextRow(){
+    app.nextRow();
 }
 
 function nextPage(){
-    
-    data.nextPage();
-    m.mount(root, view);
+    app.nextPage();
 }
 
 function lastPage(){
-    
-    data.lastPage();
-    m.mount(root, view);
+    app.lastPage();
 }
 
 function prevRow() {
-    data.prevRow();
-    m.mount(root, view);
+    app.prevRow();
 }
 function prevPage() {
-    data.prevPage();
-    m.mount(root, view);
+    app.prevPage();
 }
 function firstPage() {
-    data.firstPage();
-    m.mount(root, view);
+    app.firstPage();
 }
 
-function getNumVal(id){
-    return Number(document.getElementById(id).value)
-}
+// handler for 'page size' value changed
 
 function pageSizeChanged () {
-    data.pageSize = getNumVal('pagesize');
-    m.mount(root, view);
+    app.pageSize = Number(document.getElementById('pagesize').value);
 }
 
+// handler for 'start' value changed
+
 function startChanged() {
-    data.start = getNumVal('start');
-    m.mount(root, view);
+    app.start = Number(document.getElementById('start').value);
 }
+
+//  create a row from a given data point
 
 function tableRow(v) {
     return m('tr', m('td', v.ind), m('td', v.locn), m('td', v.ordid), m('td', v.ksn), m('td', v.sku));
@@ -122,20 +73,78 @@ var upperControls = m('ww',
         m('button', {onclick: prevPage}, "<<"),
         m('button', {onclick: firstPage}, "<<<"),
         m('strong', 'Showing'), m('input', {id : 'pagesize', value: '10', onchange:pageSizeChanged}, '10'), 
-        m('strong', 'rows out of'), m('input', {value: data.max}, ''),
+        m('strong', 'rows out of'), m('input', {value: data.data.length}, ''),
         m('strong', 'starting at row'), m('input', {id : 'start', value: 1, onchange:startChanged}, ''),
         m('button', {onclick: nextRow}, ">"),
         m('button', {onclick: nextPage}, ">>"),
         m('button', {onclick: lastPage}, '>>>')
     );
 
-var view = {
+var app = {
     
+    start : 1,
+    pageSize : 10,
+    max : data.data.length,
+
     view: function () {
-        return m('main', upperControls , m('table',  tableHeader, data.getViewData().map(tableRow)));
+        return m('main', upperControls , m('table', tableHeader, this.getViewData().map(tableRow)));
+    },
+
+    // update control value
+    setStart: function(){
+        document.getElementById('start').value = this.start;
+    },
+
+     // return a subset of objects based on current value of 'start' and 'page size' 
+
+     getViewData : function() {
+        
+        var viewData = [];
+        var end = Math.min(this.start -1 + this.pageSize, this.max);
+
+        for(var k=this.start-1; k < end; k++){
+            viewData.push(data.data[k]);
+        }
+        return viewData;
+    },
+
+    nextRow : function(){
+        this.start = Math.min(this.start + 1, this.max);
+        this.setStart();
+    },
+
+    prevRow : function(){
+        this.start = Math.max(this.start - 1, 1);
+        this.setStart();
+    },
+    
+    nextPage: function(){
+
+        this.start += this.pageSize;
+        if (this.start > this.max){
+            this.start -= this.pageSize;
+        }
+        this.setStart();
+    },
+
+    prevPage: function() {
+        this.start -= this.pageSize;
+        if (this.start < 1){
+            this.start += this.pageSize;
+        }
+        this.setStart();
+    },
+    
+    lastPage: function() {
+        this.start = Math.max(this.max - this.pageSize + 1, 1);
+        this.setStart();
+    },
+
+    firstPage: function(){
+        this.start = 1;
+        this.setStart();
     }
 };
 
 var root = document.body;
-
-m.mount(root, view);
+m.mount(root, app);
